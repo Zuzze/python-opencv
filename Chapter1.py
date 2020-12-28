0e96bd041ac127e1b1598d897078981b92b48e34
Zuzze <nevalsus@gmail.com>
2020-12-29 00:36:54 +0100

    webcam brigtness adjusted

--- Chapter1.py
+++ Chapter1.py
@@ -27,11 +27,14 @@ while True:
 # param for VideoCapture is the id of web cam
 # in windows this is apparently 0, on mac this was 1
 # press q to quit the stream
+# see more at https://docs.opencv.org/3.4/d8/dfe/classcv_1_1VideoCapture.html
 cap = cv2.VideoCapture(1)
 # set webcam width (id=3)
 cap.set(3, 640)
 # set webcam height (id=4)
 cap.set(4, 480)
+# set brightness (id=10)
+cap.set(10, 100)
 
 while True:
     success, img = cap.read()
